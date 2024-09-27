430d5e4320da01b62b8f499406a32dfef5466c16
leejonghyun93 <fhohffodf93@gmail.com>
2024-09-27 21:10:43 +0900

    API Gateway 및 filter 적용(Custom,Global,Logging)

---
+++ src/main/java/com/example/toymsa/ToyMsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.toymsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toymsa/ToyMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toymsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
